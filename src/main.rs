60942f6dcb5954c50fbc52841d86836acd3af761
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-28 18:23:39 -0300

    allow custom ttl and mc_monitor usage at runtime

--- src/main.rs
+++ src/main.rs
@@ -14,31 +14,47 @@ use axum::{
 use color_eyre::Result;
 use moka::future::{Cache, CacheBuilder};
 use serde::Serialize;
-use std::{env, net::SocketAddr, sync::Arc, time::Duration};
+use std::{env, net::SocketAddr, sync::Arc};
 use tokio::process::Command;
-use tracing::{debug, warn};
+use tracing::{info, debug, warn, Instrument};
+use tracing::debug_span;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 struct AppState {
     mc_monitor_executable: Arc<str>,
+    use_mc_monitor: Arc<bool>,
     cache: Cache<String, ServerStatus>,
 }
 
 impl AppState {
     fn new() -> Self {
         const MC_MONITOR_EXECUTABLE: &str = "MC_MONITOR_EXECUTABLE";
+        const CACHE_TTL: &str = "CACHE_TTL";
+        const USE_MC_MONITOR: &str = "USE_MC_MONITOR";
 
         let mc_monitor_executable = env::var(MC_MONITOR_EXECUTABLE)
             .unwrap_or_else(|_| "mc-monitor".to_owned())
             .into();
 
+        let cache_ttl = env::var(CACHE_TTL).unwrap_or_else(|_| "10 seconds".to_owned());
+        let cache_ttl = parse_duration::parse(&cache_ttl)
+            .unwrap_or_else(|_| panic!("Expected string {cache_ttl} to be a duration"));
+
+        let use_mc_monitor = env::var(USE_MC_MONITOR)
+            .unwrap_or_else(|_| "true".to_owned())
+            .parse::<bool>()
+            .unwrap_or_else(|_| panic!("Failed parsing variable {USE_MC_MONITOR} into bool"))
+            .into();
+
+        info!(%mc_monitor_executable);
+        info!(%use_mc_monitor);
+        info!(?cache_ttl);
+
         Self {
             mc_monitor_executable,
-            cache: CacheBuilder::new(100)
-                .time_to_live(Duration::from_secs(30))
-                .time_to_idle(Duration::from_secs(15))
-                .build(),
+            cache: CacheBuilder::new(100).time_to_live(cache_ttl).build(),
+            use_mc_monitor,
         }
     }
 }
@@ -51,7 +67,7 @@ struct ServerStatus {
     error: Option<String>,
 }
 
-async fn fetch_status_from_server(
+async fn fetch_status_with_mc_monitor(
     url: &str,
     mc_monitor_executable: &str,
 ) -> Result<ServerStatus, (StatusCode, String)> {
@@ -91,8 +107,7 @@ async fn fetch_status_from_server(
     let exit_code = output
         .status
         .code()
-        .expect("mc-monitor should terminate normally");
-    let exit_code = exit_code
+        .expect("mc-monitor should terminate normally")
         .try_into()
         .expect("Exit codes should fit into u8s");
 
@@ -102,6 +117,21 @@ async fn fetch_status_from_server(
         output: stdout,
         error: stderr,
     })
+}
+
+async fn fetch_status_from_server(
+    url: &str,
+    use_mc_monitor: bool,
+    mc_monitor_executable: &str,
+) -> Result<ServerStatus, (StatusCode, String)> {
+    if use_mc_monitor {
+        let span = debug_span!("mc_monitor_fetch", url = url);
+        fetch_status_with_mc_monitor(url, mc_monitor_executable)
+            .instrument(span)
+            .await
+    } else {
+        todo!()
+    }
 }
 
 async fn get_status_for_server(
@@ -115,7 +145,7 @@ async fn get_status_for_server(
 
     let status = cache
         .try_get_with_by_ref(&url, async {
-            fetch_status_from_server(&url, &mc_monitor_executable).await
+            fetch_status_from_server(&url, *state.use_mc_monitor, &mc_monitor_executable).await
         })
         .await
         .map_err(|e| (*e).clone())?;
